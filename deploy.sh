355465f3210c46f2bbc4e228e2618d537638ea8c
Mrli2016 <mrli2016@126.com>
2019-09-12 11:12:53 +0800

    发布脚本增加打印链接

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+### 
+# @Author: 李永兴
+ # @Email: mrli2016@126.com
+ # @Date: 2019-05-15 09:04:35
+ # @LastEditTime: 2019-09-12 11:09:02
+ # @Description: 
+ ###
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
@@ -21,5 +28,8 @@ git push -f https://github.com/Mrli2016/Mrli2016.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:Mrli2016/my-vuepress.git master:gh-pages
+
+echo -e "\033[34m已发布成功 可查看链接: \033[0m"
+echo -e "\033[36mhttps://mrli2016.github.io/\033[0m"
 
 cd -
