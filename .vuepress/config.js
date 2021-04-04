d46ffc990303ff148836f45cec09f88235a313e7
Mrli2016 <mrli2016@126.com>
2021-04-04 11:22:17 +0800

    更新vuepress主题

---
+++ .vuepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  "title": "Mrli2016",
+  "description": '念念不忘 必有回响',
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/Mrli2016",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    // "sidebar": {
+    //   "/docs/theme-reco/": [
+    //     "theme",
+    //     "plugin",
+    //     "api"
+    //   ]
+    // },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Mrli2016",
+    "authorAvatar": "/hero.jpg",
+    "record": "",
+    "startYear": "2019"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
